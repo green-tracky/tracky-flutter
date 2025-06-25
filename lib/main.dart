7a11deebdf0a2a5fd2dee6471841599dcf5389bd
TaengGyul <mjcho0130@gmail.com>
2025-06-25 10:40:55 +0900

    시작 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tracky_flutter/ui/pages/community/challenge/list_page.dart';
 import 'package:tracky_flutter/ui/pages/community/leaderboard/main_page/main_page.dart';
+import 'package:tracky_flutter/ui/pages/community/post/list_page/list_page.dart';
 import 'package:tracky_flutter/ui/widgets/common_bottom_nav.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         '/plan': (context) => const DummyPage(title: '플랜 페이지'),
         '/running': (context) => const DummyPage(title: '러닝 페이지'),
         '/community': (context) => const DummyPage(title: '커뮤니티', child: ChallengeListPage()),
+        '/post': (context) => const DummyPage(title: '게시글', child: PostListPage()),
         '/activity': (context) => const DummyPage(title: '활동', child: LeaderboardView()),
       },
     );
@@ -35,4 +37,3 @@ class DummyPage extends StatelessWidget {
     );
   }
 }
-
