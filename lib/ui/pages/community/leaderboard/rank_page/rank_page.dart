f19af217d5a62678a023dcb2977e0301c535c1a8
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-06-30 11:19:55 +0900

    import edited

--- lib/ui/pages/community/leaderboard/rank_page/rank_page.dart
+++ lib/ui/pages/community/leaderboard/rank_page/rank_page.dart
@@ -6,24 +6,14 @@ import 'package:tracky_flutter/ui/pages/community/leaderboard/rank_page/widgets/
 import 'package:tracky_flutter/ui/pages/community/leaderboard/rank_page/widgets/rank_leader_board.dart';
 import 'package:tracky_flutter/ui/pages/community/leaderboard/rank_page/widgets/rank_text.dart';
 import 'package:tracky_flutter/ui/pages/community/leaderboard/rank_page/widgets/rank_user_tile.dart';
-import 'package:tracky_flutter/ui/pages/community/widgets/common_appbar.dart';
-import 'package:tracky_flutter/ui/pages/community/widgets/common_drawer.dart';
+import 'package:tracky_flutter/ui/widgets/common_appbar.dart';
+import 'package:tracky_flutter/ui/widgets/common_drawer.dart';
 
 class RankPage extends ConsumerWidget {
   const RankPage({super.key});
 
-  void showFilterBottomSheet(
-    BuildContext context,
-    WidgetRef ref,
-    String selected,
-  ) {
-    final options = [
-      '이번 주 친구 기록(KM)',
-      '지난 주 친구 기록(KM)',
-      '이번 달 친구 기록(KM)',
-      '지난 달 친구 기록(KM)',
-      '올해 친구 기록(KM)',
-    ];
+  void showFilterBottomSheet(BuildContext context, WidgetRef ref, String selected) {
+    final options = ['이번 주 친구 기록(KM)', '지난 주 친구 기록(KM)', '이번 달 친구 기록(KM)', '지난 달 친구 기록(KM)', '올해 친구 기록(KM)'];
 
     RankFilter(context, options, selected, ref);
   }
@@ -35,20 +25,14 @@ class RankPage extends ConsumerWidget {
 
     return Scaffold(
       backgroundColor: Color(0xFFF9FAEB),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(90),
-        child: CommonAppBar(),
-      ),
+      appBar: PreferredSize(preferredSize: Size.fromHeight(90), child: CommonAppBar()),
       endDrawer: CommunityDrawer(),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           RankLeaderBoard(),
           Divider(thickness: 1),
-          RankHeader(
-            selected,
-            () => showFilterBottomSheet(context, ref, selected),
-          ),
+          RankHeader(selected, () => showFilterBottomSheet(context, ref, selected)),
           Divider(thickness: 1),
           RankText(),
           Divider(thickness: 1),
