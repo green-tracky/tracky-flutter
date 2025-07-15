9fb9f7f8657f6e1833a5c8a468e983802c1477bd
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-07-15 11:29:27 +0900

    통신완료

--- lib/ui/pages/run/main_page/main_page.dart
+++ lib/ui/pages/run/main_page/main_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geolocator/geolocator.dart'; // 위치 패키지 추가!
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:tracky_flutter/_core/constants/theme.dart';
 import 'package:tracky_flutter/ui/pages/run/main_page/main_page_vm.dart';
 import 'package:tracky_flutter/ui/pages/run/main_page/widgets/run_goal_setting_button.dart';
@@ -8,7 +12,6 @@ import 'package:tracky_flutter/ui/pages/run/main_page/widgets/run_main_appbar.da
 import 'package:tracky_flutter/ui/pages/run/main_page/widgets/run_main_title.dart';
 import 'package:tracky_flutter/ui/pages/run/main_page/widgets/run_map_section.dart';
 import 'package:tracky_flutter/ui/pages/run/main_page/widgets/run_start_button.dart';
-import 'package:tracky_flutter/ui/pages/run/running_page/running_page_vm.dart';
 
 class RunMainPage extends ConsumerStatefulWidget {
   const RunMainPage({super.key});
@@ -18,18 +21,46 @@ class RunMainPage extends ConsumerStatefulWidget {
 }
 
 class _RunMainPageState extends ConsumerState<RunMainPage> {
+  StreamSubscription<Position>? _locationSub;
 
   @override
   void initState() {
     super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final state = ref.read(runRunningProvider);
-      if (state is AsyncData && state.value != null && state.value!.isRunning) return;
-
-      ref.read(runRunningProvider.notifier).startNewRun(1);
-    });
+    _requestPermissionAndListen();
   }
+
+  Future<void> _requestPermissionAndListen() async {
+    // 권한 요청
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+        return; // 권한 거부 시 위치 구독 안함
+      }
+    }
+
+    // 위치 스트림 구독
+    _locationSub =
+        Geolocator.getPositionStream(
+          locationSettings: const LocationSettings(
+            accuracy: LocationAccuracy.high,
+            distanceFilter: 3, // 최소 이동 거리(m)마다 업데이트 (선택)
+          ),
+        ).listen((Position pos) {
+          ref
+              .read(runMainProvider.notifier)
+              .updateLocation(
+                LatLng(pos.latitude, pos.longitude),
+              );
+        });
+  }
+
+  @override
+  void dispose() {
+    _locationSub?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
