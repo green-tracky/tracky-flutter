7b0e8adec53a1a75362985a3c973592353d9fe75
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-07-16 15:24:53 +0900

    [Refactor] 친구 정보 불러오기 및 통신 코드 변경 진행 중 (#62)
    * import edited
    
    * 컴포넌트 분리 완료
    
    * 컴포넌트 분리
    
    * 목업
    
    * 하는중
    
    * ã로직
    
    * 하는중
    
    * 서버 직전
    
    * 통신중
    
    * 서버연동
    
    * 수정
    
    * 친구 더미 생성
    
    * 하는중
    
    * 통신완료
    
    * ãmerge 완
    
    * 통신
    
    * 끝
    
    * 챌린지 통신
    
    ---------
    
    Co-authored-by: Sxias <106750460+Sxias@users.noreply.github.com>

--- lib/ui/pages/activity/detail_page/intensity/intensity_page.dart
+++ lib/ui/pages/activity/detail_page/intensity/intensity_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tracky_flutter/ui/pages/activity/detail_page/detail_vm.dart';
+import 'package:tracky_flutter/ui/pages/run/run_vm.dart';
 
 class IntensityPage extends ConsumerStatefulWidget {
   const IntensityPage({super.key});
